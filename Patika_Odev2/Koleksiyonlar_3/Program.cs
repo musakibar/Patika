57da08805342b00147b1cb2a2090630213da199d
musakibar <kibar378@gmail.com>
2022-09-02 22:54:07 +0300

    Koleksiyonlar_3

---
+++ Patika_Odev2/Koleksiyonlar_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+
+
+Console.WriteLine("Cümle Giriniz.");
+string sentence = Console.ReadLine().ToLower();
+
+List<char> sesliHarfler = new List<char>();
+
+List<char> vowels = new List<char>() {'a','e','i','o','u' };
+
+for (int i = 0; i < sentence.Length; i++)
+{
+    if (vowels.Contains(sentence[i]))
+    {
+        sesliHarfler.Add(sentence[i]);
+    }
+}
+
+foreach (var item in sesliHarfler)
+{
+    Console.WriteLine(item);
+}
