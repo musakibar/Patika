acc8a2fe44ff3e0008881f057614d6803ae3500b
musakibar <kibar378@gmail.com>
2022-09-04 01:59:49 +0300

    ToDo

---
+++ Patika_ToDo/PersonInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoUygulaması
+{
+    public class PersonInfo
+    {
+        public static List<Person> person = new List<Person>();
+
+        public PersonInfo()
+        {
+            person.Add(new Person(1, "Musa Kibar"));
+            person.Add(new Person(2, "Ali Nurkoyuncu"));
+            person.Add(new Person(3, "Alperen Şener"));
+            person.Add(new Person(4, "Mehmet Mutlu"));
+        }
+    }
+}
